775b8d133e74038f91d81854500da7c556d33329
Vansh Badjate <128991304+vanshbadjate07@users.noreply.github.com>
2025-11-05 20:05:23 +0530

    Update API_BASE to use window.location.origin

--- static/script.js
+++ static/script.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = window.location.origin;
 
 const chatEl = document.getElementById("chat");
 const inputEl = document.getElementById("userInput");
